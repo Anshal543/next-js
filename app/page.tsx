1863f7038eb7871eb1ca22f16396480527cb9764
Anshal Ali <sherhasan543@gmail.com>
2024-09-09 00:02:37 +0500

    supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,41 @@
+import { createClient } from "@/supabase/client";
 import Card from "@/components/card";
 
-export default function Home() {
+export default async function Home() {
   const products = [
     {
-      id:0,
-      name:"Mushroom Orange Lamp",
-      description:"Mushroom Orange Lamp desc",
+      id: 0,
+      name: "Mushroom Orange Lamp",
+      description: "Mushroom Orange Lamp desc",
       price: 100,
-      imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPMFVKYfJpJazShIE-HurrHpLp-dO4COPDrQ&s"
-    }
-  ]
+      imageUrl:
+        "http://res.cloudinary.com/anshal/image/upload/v1725383949/kwx7etferjsufhumozom.jpg",
+    },
+  ];
+  const supabase = createClient();
+  const { data, error } = await supabase.from("easysell=products").select();
+
   return (
-      <main className="min-h-screen mx-auto max-w-[100rem]">
-        {products.map(product=>(
-          <Card key={`${product.name}-${product.id}`} {...product}/>
-        ))}
-      </main>
+    <main className="min-h-screen mx-auto max-w-[100rem]">
+      <div className="px-12 pt-12 pb-20">
+        <div className="flex flex-col xl:flex-row gap-2 xl:gap-40">
+          <div className="pt-12">
+            <h2 className="text-4xl mb-16">OUR TOP PRODUCTS</h2>
+            <p className="text-xl">You can pay to boost your products here.</p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 xl:gap-12">
+            {products.map((product) => (
+              <Card key={`${product.name}-${product.id}`} {...product} />
+            ))}
+          </div>
+        </div>
+        <h2 className="text-4xl mt-20 mb-16">ALL PRODUCTS</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          {products.map((product) => (
+            <Card key={`${product.name}-${product.id}`} {...product} />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
--- components/card.tsx
+++ components/card.tsx
@@ -6,6 +6,7 @@ interface CardProps {
   imageUrl: string;
 }
 
+import Image from "next/image";
 import React from "react";
 
 const Card: React.FC<CardProps> = ({
@@ -18,7 +19,15 @@ const Card: React.FC<CardProps> = ({
   return (
     <div className="max-w-lg bg-gray-953 rounded-sm overflow-hidden h-full flex flex-col justify-between">
       <div>
-        <div className="relative h-96 w-[500px] bg-center"></div>
+        <div className="relative h-64  bg-center">
+          <Image
+          src={imageUrl}
+          style={{objectFit:"cover"}}
+          layout="fill"
+          alt={name}
+          className="rounded-t"
+          />
+        </div>
         <div className="px-6 py-4">
           <div className="text-2xl mb-2 uppercase line-clamp-2 ">{name}</div>
           <p className="text-gray-700 text-base truncate uppercase">
