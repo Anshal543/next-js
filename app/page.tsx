fa27ebbc83cf3a8374ccf60c9ef5469b076c5c89
Anshal Ali <sherhasan543@gmail.com>
2024-09-11 22:28:15 +0500

    Refactor page component, add sellYourItem function, and create UploadFormPage component

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default async function Home() {
   const { data: products, } = await supabase
     .from("easysell-products")
     .select();
-  // console.log(data);
   if (!products) {
     return notFound()
   }
